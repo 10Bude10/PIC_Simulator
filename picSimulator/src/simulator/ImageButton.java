f02166418be18fe9ded433a51cc5e6d326bc597d
Felix_000 <Felix_000@FoffisUltrabook>
2013-04-29 14:51:38 +0200

    Working on the LED Array

--- picSimulator/src/simulator/ImageButton.java
+++ picSimulator/src/simulator/ImageButton.java
@@ -22,39 +22,51 @@ public class ImageButton extends AbstractButton {
 	   *  Customized Icon with new functions
 	   */
 	  public ImageButton(String imagePath, String imageMOPath, int widthImg, int heightImg) {
-		  this.width = widthImg;
-		  this.height = heightImg;
-		 	
-		  try {  
-			  img = ImageIO.read(ClassLoader.getSystemResource(imagePath));
-			  imgMO = ImageIO.read(ClassLoader.getSystemResource(imageMOPath));
-		  } catch(Exception e) {
-			  e.getMessage();
-		  }
-		  setPreferredSize(new Dimension(width, height));  
-		  setIcon(new ImageIcon(img.getScaledInstance(width, height, Image.SCALE_SMOOTH)));
-		  setRolloverEnabled(true);
-		  setRolloverIcon(new ImageIcon(imgMO.getScaledInstance(width, height, Image.SCALE_SMOOTH)));
-		  
-		  
-			  
-			  
-			  /*		   if((img != null) && (imgMO != null)) {
-			  setPreferredSize(new Dimension(width, height));  
-			  scaledImg = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-			  scaledImgMO = imgMO.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-			  Graphics g = imgMO.getGraphics();
-			  g.drawImage(scaledImgMO, 0, 0, width, height, null);
-		  }
-		  
-		  
-	  }  
-	  
-	  protected void paintComponent(Graphics g) {  
-		  super.paintComponent(g); 
-		  if(true) {
-			  g.drawImage(scaledImgMO, 0, 0, width, height, null);
-		  }
-*/	  }
+			
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+			private int width = 50;
+			private int height = 50;
+			private String path = "./resources/";
 
-}
+			/*
+			 * Customized Icon with new functions
+			 */
+		public ImageButton(String imagePath, String imageMOPath, String tooltip) {
+				Image ImageNormal = getToolkit().getImage(path + imagePath).getScaledInstance(width, height, Image.SCALE_SMOOTH);
+				Image ImageMouseOver = getToolkit().getImage(path + imageMOPath).getScaledInstance(width, height, Image.SCALE_SMOOTH);
+				
+				ImageIcon iconNormal = new ImageIcon(ImageNormal);
+				ImageIcon IconMouseOver = new ImageIcon(ImageMouseOver);
+				setIcon(iconNormal);
+				setRolloverIcon(IconMouseOver);
+				setText(tooltip);
+				
+				/*		try {
+					img = ImageIO.read(ClassLoader.getSystemResource(imagePath));
+					imgMO = ImageIO.read(ClassLoader.getSystemResource(imageMOPath));
+				} catch (Exception e) {
+					e.getMessage();
+				}
+						if ((img != null) && (imgMO != null)) {
+					this.setPreferredSize(new Dimension(width, height));
+					scaledImg = img
+							.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+					scaledImgMO = imgMO.getScaledInstance(width, height,
+							Image.SCALE_SMOOTH);
+					Graphics g = img.getGraphics();
+					g.drawImage(scaledImg, 0, 0, width, height, null);
+				}
+			}
+
+			protected void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				if (true) {
+					g.drawImage(scaledImg, 0, 0, width, height, null);
+				}
+		*/	} 
+
+	}
+
